c7004d0e32747b90653ad1a19368171503c41452
pramodWijesinghe <pramod@yaludev.com>
2026-01-15 14:36:40 +0530

    ui changes

--- src/app/billing/page.js
+++ src/app/billing/page.js
@@ -2,9 +2,7 @@ import { BreadcrumbProvider } from '@/hooks/providers/useBreadcrumbProvider'
 import React from 'react'
 
 const items = [
-  // { label: "Dashboard", href: "/dashboard" },
   { label: "Billing", href: "/billing" },
-//   { label: "Customer", href: null },
 ];
 
 export default function page() {
