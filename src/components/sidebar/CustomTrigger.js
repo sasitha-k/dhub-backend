7a878d9d3283dab3108a734cfc010e7923815740
pramodWijesinghe <pramod@yaludev.com>
2026-02-08 11:08:12 +0530

    bugfix

--- src/components/sidebar/CustomTrigger.js
+++ src/components/sidebar/CustomTrigger.js
@@ -1,14 +1,14 @@
 'use client'
 
 import { useSidebar } from "@/components/ui/sidebar"
-import { PanelLeft } from "lucide-react"
+import { Menu, SquareMenu } from "lucide-react"
 
 export function CustomTrigger() {
   const { toggleSidebar } = useSidebar()
 
     return <button onClick={toggleSidebar}>
         <div className="p-3 hover:bg-primary/30 m-1 rounded-xl">
-            <PanelLeft className="w-7 h-7"/>
+            <Menu className="w-7 h-7"/>
       </div>
   </button>
 }
--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -43,7 +43,7 @@ function useSidebar() {
 }
 
 function SidebarProvider({
-  defaultOpen = false,
+  defaultOpen = true,
   open: openProp,
   onOpenChange: setOpenProp,
   className,
